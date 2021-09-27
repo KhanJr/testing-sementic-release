f700440afe280a2647bf7c5d25edc3e5449eb257
KhanJr <khajr@gmail.com>
2021-09-27 15:33:31 +0530

    adding different feature

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   plugins: ["commitlint-plugin-jira-rules"],
-  extends: ["jira"],
   rules: {
-    "jira-task-id-separator": [0],
-    "jira-task-id-case": [0],
-    "jira-task-id-min-length": [0],
-    "jira-commit-status-case": [0],
-    "header-max-length": [0],
-    "jira-task-id-max-length": [0],
     "scope-case": [
       2,
       "always",
