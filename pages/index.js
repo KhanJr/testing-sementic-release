514cc11f2c1cd338c019124dafd22b02032cfe11
KhanJr <khajr@gmail.com>
2021-09-27 16:22:12 +0530

    fix(.commitlintrc): fixed the rules

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -7,6 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <h1>Testing semantic release</h1>
       <h2>adding another line</h2>
+      <h3>testing another rule</h3>
     </div>
   );
 }
